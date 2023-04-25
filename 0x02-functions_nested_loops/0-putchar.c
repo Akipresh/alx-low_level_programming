6ca0e707b77aad86a9e1465db6a01e64852bb369
akipresh <akivieprecious@gmail.com>
2023-04-25 14:01:04 -0700

    loop task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,15 +4,3 @@
  * A program to print _putchar followed by a line
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
