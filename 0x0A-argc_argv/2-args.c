e1c2b36683c366ba1e0d0fe27cd68e2468b615e1
akipresh <akivieprecious@gmail.com>
2023-05-15 15:56:12 -0700

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - a program that multiply two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int k;
+
+	for (k = 0; k < argc; k++)
+		printf("%s\n", argv[k]);
+
+	return (0);
+}
