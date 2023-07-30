42bdd27a8204f57b816fb6267ebe023480928b66
akipresh <akivieprecious@gmail.com>
2023-07-29 17:12:16 -0700

    {task}

---
+++ 0x18-dynamic_libraries/custom_gm.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * lottery_draw: Function to overide the lottery_draw
+ * Return: void
+ */
+
+void lottery_draw(void)
+{
+	printf("--> Please make me win!");
+}
